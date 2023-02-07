663960d19e77fa8907a201d778bee3480eb55123
Nickont <s2063346@ed.ac.uk>
2023-02-07 18:09:52 +0000

    Update number_detector_yolo.py

--- Computer Vision/number_detector_yolo.py
+++ Computer Vision/number_detector_yolo.py
@@ -20,9 +20,9 @@ from matplotlib import use as mpl_use
 cardW=57
 cardH=87
 cornerXmin=2
-cornerXmax=10.5
-cornerYmin=2.5
-cornerYmax=23
+cornerXmax=9
+cornerYmin=5.5
+cornerYmax=24.5
 
 # We convert the measures from mm to pixels: multiply by an arbitrary factor 'zoom'
 # You shouldn't need to change this
@@ -123,7 +123,7 @@ def extract_card(img, output_fn=None, min_focus=120, debug=False):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Noise-reducing and edge-preserving filter
-    gray = cv2.bilateralFilter(gray, 11, 17, 17)
+    gray = cv2.bilateralFilter(gray, 2, 157, 157)
 
     # Edge extraction
     edge = cv2.Canny(gray, 30, 200)
@@ -198,31 +198,144 @@ def extract_card(img, output_fn=None, min_focus=120, debug=False):
 
 # Test on one image
 debug=False
-img=cv2.imread("example_cards/ace_of_spades.jpg")
+img=cv2.imread("example_cards/6hearts.jpg")
+#img=cv2.imread("example_cards/ace_of_spades.jpg")
 display_img(img)
-valid,card=extract_card(img,"example_cards/ace_of_spades3.jpg", debug=debug)
+#valid,card=extract_card(img,"example_cards/ace_of_spades.jpg", debug=debug)
+valid,card=extract_card(img,"example_cards/6hearts.jpg", debug=debug)
 if valid:
     display_img(card)
 if debug:
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-"""img=cv2.imread("example_cards/ace_of_spades.jpg")
+#img=cv2.imread("example_cards/ace_of_spades.jpg")
+img=cv2.imread("example_cards/6hearts.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.bilateralFilter(gray, 11, 17, 17)
+gray = cv2.bilateralFilter(gray, 2, 157, 157)
 edges = cv2.Canny(gray, 30, 200)
 #edges = cv2.Canny(img,100,200)
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(edges,cmap = 'gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-plt.show()"""
+plt.show()
 
 
 # Run a few times...
 """imgs_dir="data/cards"
 imgs_fns=glob(imgs_dir+"/*/*.png")
-img_fn=random.choice(imgs_fns)
-display_img(cv2.imread(img_fn,cv2.IMREAD_UNCHANGED),polygons=[refCornerHL,refCornerLR])"""
+img_fn=random.choice(imgs_fns)"""
+img=cv2.imread("example_cards/6hearts.jpg")
+display_img(cv2.imread("example_cards/6hearts.jpg"),polygons=[refCornerHL,refCornerLR])
 
 
+def findHull(img, corner=refCornerHL, debug="no"):
+    """
+        Find in the zone 'corner' of image 'img' and return, the convex hull delimiting
+        the value and suit symbols
+        'corner' (shape (4,2)) is an array of 4 points delimiting a rectangular zone,
+        takes one of the 2 possible values : refCornerHL or refCornerLR
+        debug=
+    """
+
+    kernel = np.ones((3, 3), np.uint8)
+    corner = corner.astype(int)
+
+    # We will focus on the zone of 'img' delimited by 'corner'
+    x1 = int(corner[0][0])
+    y1 = int(corner[0][1])
+    x2 = int(corner[2][0])
+    y2 = int(corner[2][1])
+    w = x2 - x1
+    h = y2 - y1
+    zone = img[y1:y2, x1:x2].copy()
+
+    strange_cnt = np.zeros_like(zone)
+    gray = cv2.cvtColor(zone, cv2.COLOR_BGR2GRAY)
+    thld = cv2.Canny(gray, 30, 200)
+    thld = cv2.dilate(thld, kernel, iterations=1)
+    if debug != "no": cv2.imshow("thld", thld)
+
+    # Find the contours
+    contours, _ = cv2.findContours(thld.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    min_area = 80  # We will reject contours with small area. TWEAK, 'zoom' dependant
+    min_solidity = 1.8  # Reject contours with a low solidity. TWEAK
+
+    concat_contour = None  # We will aggregate in 'concat_contour' the contours that we want to keep
+
+    ok = True
+    for c in contours:
+        area = cv2.contourArea(c)
+
+        hull = cv2.convexHull(c)
+        hull_area = cv2.contourArea(hull)
+        solidity = float(area) / hull_area
+        # Determine the center of gravity (cx,cy) of the contour
+        M = cv2.moments(c)
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+        #  abs(w/2-cx)<w*0.3 and abs(h/2-cy)<h*0.4 : TWEAK, the idea here is to keep only the contours which are closed to the center of the zone
+        if area >= min_area and abs(w / 2 - cx) < w * 0.3 and abs(h / 2 - cy) < h * 0.4 and solidity > min_solidity:
+            if debug != "no":
+                cv2.drawContours(zone, [c], 0, (255, 0, 0), -1)
+            if concat_contour is None:
+                concat_contour = c
+            else:
+                concat_contour = np.concatenate((concat_contour, c))
+        if debug != "no" and solidity <= min_solidity:
+            print("Solidity", solidity)
+            cv2.drawContours(strange_cnt, [c], 0, 255, 2)
+            cv2.imshow("Strange contours", strange_cnt)
+
+    if concat_contour is not None:
+        # At this point, we suppose that 'concat_contour' contains only the contours corresponding the value and suit symbols
+        # We can now determine the hull
+        hull = cv2.convexHull(concat_contour)
+        hull_area = cv2.contourArea(hull)
+        # If the area of the hull is to small or too big, there may be a problem
+        min_hull_area = 940  # TWEAK, deck and 'zoom' dependant
+        max_hull_area = 1120  # TWEAK, deck and 'zoom' dependant
+        if hull_area < min_hull_area or hull_area > max_hull_area:
+            ok = False
+            if debug != "no":
+                print("Hull area=", hull_area, "too large or too small")
+        # So far, the coordinates of the hull are relative to 'zone'
+        # We need the coordinates relative to the image -> 'hull_in_img'
+        hull_in_img = hull + corner[0]
+
+    else:
+        ok = False
+
+    if debug != "no":
+        if concat_contour is not None:
+            cv2.drawContours(zone, [hull], 0, (0, 255, 0), 1)
+            cv2.drawContours(img, [hull_in_img], 0, (0, 255, 0), 1)
+        cv2.imshow("Zone", zone)
+        cv2.imshow("Image", img)
+        if ok and debug != "pause_always":
+            key = cv2.waitKey(1)
+        else:
+            key = cv2.waitKey(0)
+        if key == 27:
+            return None
+    if ok == False:
+        return None
+
+    return hull_in_img
+
+# Test find_hull on a random card image
+# debug = "no" or "pause_always" or "pause_on_pb"
+# If debug!="no", you may have to press a key to continue execution after pause
+
+
+#img=cv2.imread("example_cards/ace_of_spades.jpg",cv2.IMREAD_UNCHANGED)
+"""img=cv2.imread("example_cards/6hearts.jpg",cv2.IMREAD_UNCHANGED)
+
+
+hullHL=findHull(img,refCornerHL,debug=debug)
+hullLR=findHull(img,refCornerLR,debug=debug)
+display_img(img,[refCornerHL,refCornerLR,hullHL,hullLR])
+
+if debug!="no": cv2.destroyAllWindows()"""
